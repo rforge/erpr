97629444bbebf705188a411b41d8c111473bf543
giorgioarcara <giorgioarcara@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-03-19 09:18:15 +0000

    delete 3
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/erpr@15 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/export.bplot.R
+++
@@ -1,9 +0,0 @@
-export.bplot <-
-function(dat, Npts=dim(dat)[1], startmsec=-200, endmsec=1200, SB=1, SC=500, outname="Bplotfile.avr")
-	{
-	TSB=startmsec
-	DI=(endmsec+abs(startmsec))/Npts # distanza in ms tra un punto e l'altro della sweep.
-	dat=t(dat)
-	write(paste("Npts=",Npts, "TSB=",TSB, "DI=",DI,"SB=",SB, "SC=",SC, sep=" "), file=outname)
-	write.table(dat, row.names=F, col.names=F, file=outname, append=TRUE)
-	}
