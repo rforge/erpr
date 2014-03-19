3eabe3970ca863189ed9395f31e6dab34e8d30c7
giorgioarcara <giorgioarcara@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-03-19 09:32:35 +0000

    add erp.functions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/erpr@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/erp.latency.R
@@ -0,0 +1,53 @@
+erp.latency <-
+function(base, numbers, win.ini, win.end, env=.GlobalEnv, startmsec=-200, endmsec=1200, others=NULL, format="long", name.dep="Dep", name.newvar="Electrode", peak.fun=max, ...)
+	{	
+		datall=NULL		
+			
+		for (i in 1:length(numbers))
+		{
+		average.temp=eval(parse(file="", text=paste(base,numbers[i], sep="")),env=env)
+		rownames(average.temp)=1:dim(average.temp)[1]
+		Subject_name=comment(eval(parse(file="", text=paste(base,numbers[i], sep="")),env=env))
+		average.temp.peak=apply(average.temp[round(msectopoints(win.ini,dim(average.temp)[1],startmsec, endmsec)):round(msectopoints(win.end,dim(average.temp)[1],startmsec, endmsec)),],2, FUN=function(x){peak.fun(x, ...)})
+		
+		peak.pnts=NULL
+		for (k in 1:length(average.temp)){
+			
+			if (!is.na(average.temp.peak[k])){ # the peak point is not searched if not found previously.
+			
+				peak.pnts.temp=as.numeric(rownames(average.temp[average.temp[,k]==average.temp.peak[k],]))
+				peak.pnts[k]=peak.pnts.temp[peak.pnts.temp>=round(msectopoints(win.ini,dim(average.temp)[1],startmsec, endmsec))&peak.pnts.temp<=round(msectopoints(win.end,dim(average.temp)[1],startmsec, endmsec))][1]
+			} else {
+			peak.pnts[k]=NA
+			}
+			
+			}
+			
+		peak.msec=pointstomsec(peak.pnts, dim(average.temp)[1], startmsec, endmsec)
+		average.temp.lat=data.frame(t(peak.msec))
+		names(average.temp.lat)=names(average.temp)#ripristino i nomi degli elettrodi, persi nei pasaggi
+		average.temp.lat$Subject=numbers[i]
+		average.temp.lat$Subject_name=Subject_name
+		datall=rbind(datall, average.temp.lat)
+		}
+		rownames(datall)=1:dim(datall)[1]
+		
+	if (format=="wide"){
+		return(datall)
+	}
+	
+	if(format=="long"){
+		
+		temp=rearrange(deps=1:(length(datall)-2),oth=c("Subject", "Subject_name"),dataset=datall, name.dep=name.dep, name.newvar=name.newvar)
+		# notice: length(datall)-2 because the last two columns are Subject and Subject_name
+	}
+	
+	if (!is.null(others))
+		{
+			for (i in 1:length(others))
+			{
+			temp[, names(others)[i]]=others[i]
+			}
+		}
+	return(temp)
+	}
---
+++ pkg/R/erp.peak.R
@@ -0,0 +1,35 @@
+erp.peak<-function(base, numbers, win.ini, win.end, env=.GlobalEnv, startmsec=-200, endmsec=1200, others=NULL, format="long", name.dep="Dep", name.newvar="Electrode", peak.fun=max, ...)
+	{
+	datall=NULL		
+		for (i in 1:length(numbers))
+			{
+			x.temp=eval(parse(file="", text=paste(base,numbers[i], sep="")),env=env)
+			Subject_name=comment(eval(parse(file="", text=paste(base,numbers[i], sep="")),env=env))
+			x.temp=apply(x.temp[round(msectopoints(win.ini,dim(x.temp)[1],startmsec, endmsec)):round(msectopoints(win.end,dim(x.temp)[1],startmsec, endmsec)),],2, FUN=function(x){peak.fun(x, ...)})
+			x.temp=data.frame(t(x.temp))
+			x.temp$Subject=numbers[i]
+			x.temp$Subject_name=Subject_name
+			datall=rbind(datall, x.temp)
+			}
+		rownames(datall)=1:dim(datall)[1]
+	
+	
+	if (format=="wide"){
+		return(datall)
+	}
+	
+	if(format=="long"){
+		
+		temp=rearrange(deps=1:(length(datall)-2),oth=c("Subject", "Subject_name"),dataset=datall, name.dep=name.dep, name.newvar=name.newvar)
+		# notice: length(datall)-2 because the last two columns are Subject and Subject_name
+	}
+	
+	if (!is.null(others))
+		{
+			for (i in 1:length(others))
+			{
+			temp[, names(others)[i]]=others[i]
+			}
+		}
+	return(temp)
+	}
