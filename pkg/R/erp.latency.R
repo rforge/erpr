1f7db436fe0915949f90f64bb176c6cfbddc7c5e
giorgioarcara <giorgioarcara@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-09-27 14:12:15 +0000

    import.erp correction plus minor
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/erpr@73 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/erp.latency.R
+++ pkg/R/erp.latency.R
@@ -1,68 +1,92 @@
-erp.latency <-
-function(base, numbers, win.ini, win.end, erplist=NULL, startmsec=-200, endmsec=1200, others=NULL, format="long", name.dep="Dep", name.newvar="electrode", peak.fun=max, ...)
-	{
-	
-	# preliminary checks
-	if (is.null(erplist)){
-	stop("an erplist object containing ERP data frames must be specified!", call.=F)
-	}
-	
-	#### object checks
-	object.names=paste(base, numbers, sep="")
-	if (any(!object.names%in%names(erplist))){
-		missing.objects=object.names[!object.names%in%names(erplist)]
-		missing.object.collist=paste(missing.objects, "\n", sep="")
-		stop("The following objects are not contained in the erplist specified:\n", missing.object.collist, call.=F)
-	}
 
-	
-		datall=NULL		
-			
-		for (i in 1:length(numbers))
-		{
-		average.temp=erplist[[paste(base,numbers[i], sep="")]]
-		rownames(average.temp)=1:dim(average.temp)[1]
-		Subject_name=comment(erplist[[paste(base, numbers[i], sep="")]])
-		average.temp.peak=apply(average.temp[round(msectopoints(win.ini,dim(average.temp)[1],startmsec, endmsec)):round(msectopoints(win.end,dim(average.temp)[1],startmsec, endmsec)),],2, FUN=function(x){peak.fun(x, ...)})
-		
-		peak.pnts=NULL
-		for (k in 1:length(average.temp)){
-			
-			if (!is.na(average.temp.peak[k])){ # the peak point is not searched if not found previously.
-			
-				peak.pnts.temp=as.numeric(rownames(average.temp[average.temp[,k]==average.temp.peak[k],]))
-				peak.pnts[k]=peak.pnts.temp[peak.pnts.temp>=round(msectopoints(win.ini,dim(average.temp)[1],startmsec, endmsec))&peak.pnts.temp<=round(msectopoints(win.end,dim(average.temp)[1],startmsec, endmsec))][1]
-			} else {
-			peak.pnts[k]=NA
-			}
-			
-			}
-			
-		peak.msec=pointstomsec(peak.pnts, dim(average.temp)[1], startmsec, endmsec)
-		average.temp.lat=data.frame(t(peak.msec))
-		names(average.temp.lat)=names(average.temp)#ripristino i nomi degli elettrodi, persi nei pasaggi
-		average.temp.lat$Subject=numbers[i]
-		average.temp.lat$Subject_name=Subject_name
-		datall=rbind(datall, average.temp.lat)
-		}
-		rownames(datall)=1:dim(datall)[1]
-		
-	if (format=="wide"){
-		return(datall)
-	}
-	
-	if(format=="long"){
-		
-		temp=rearrange(deps=1:(length(datall)-2),oth=c("Subject", "Subject_name"),dataset=datall, name.dep=name.dep, name.newvar=name.newvar)
-		# notice: length(datall)-2 because the last two columns are Subject and Subject_name
-	}
-	
-	if (!is.null(others))
-		{
-			for (i in 1:length(others))
-			{
-			temp[, names(others)[i]]=others[i]
-			}
-		}
-	return(temp)
-	}
+erp.latency <-
+  function(base, numbers, win.ini, win.end, erplist=NULL, startmsec=-200, endmsec=1200, others=NULL, format="long", name.dep="Dep", name.newvar="electrode", peak.fun=max, frac = 1, ...)
+  {
+    
+    # preliminary checks
+    if (is.null(erplist)){
+      stop("an erplist object containing ERP data frames must be specified!", call.=F)
+    }
+    
+    #### object checks
+    object.names=paste(base, numbers, sep="")
+    if (any(!object.names%in%names(erplist))){
+      missing.objects=object.names[!object.names%in%names(erplist)]
+      missing.object.collist=paste(missing.objects, "\n", sep="")
+      stop("The following objects are not contained in the erplist specified:\n", missing.object.collist, call.=F)
+    }
+    
+    #### frac checks
+    if (frac > 1 | frac < 0){
+      stop("The frac argument must fall within the range 0 - 1",  call.=F)
+    }    
+    
+    datall=NULL		
+    
+    for (i in 1:length(numbers))
+    {
+      average.temp=erplist[[paste(base,numbers[i], sep="")]]
+      rownames(average.temp)=1:dim(average.temp)[1]
+      Subject_name=comment(erplist[[paste(base, numbers[i], sep="")]])
+      average.temp.peak=apply(average.temp[round(msectopoints(win.ini,dim(average.temp)[1],startmsec, endmsec)):round(msectopoints(win.end,dim(average.temp)[1],startmsec, endmsec)),],2, FUN=function(x){peak.fun(x, ...)})
+      
+      peak.pnts=NULL
+      for (k in 1:length(average.temp)){
+        
+        if (!is.na(average.temp.peak[k])){ # the peak point is not searched if not found previously.
+          
+          # I use the row.names to retrieve the indices
+          peak.pnts.temp=as.numeric(rownames(average.temp[average.temp[,k]==average.temp.peak[k],]))
+          # here I find the peak within the windows delimited by win.ini and win.end. With [1], I select the first peak found
+          # fundamental in the (unlikely) case of multiple peaks.
+          peak.pnts[k]=peak.pnts.temp[peak.pnts.temp>=round(msectopoints(win.ini,dim(average.temp)[1],startmsec, endmsec))&peak.pnts.temp<=round(msectopoints(win.end,dim(average.temp)[1],startmsec, endmsec))][1]
+        
+          #####
+          # add part with fractional peak		  
+          #########
+          # if a fractional peak is specified the algorithm search backward for 0.5
+          if (frac < 1){
+            frac.search=1:peak.pnts[k]
+            frac.peak=average.temp.peak [k] * frac
+            
+            frac.peak.pnts.temp = which.min ( abs( average.temp[ frac.search, k ] - frac.peak ) )
+            
+            # NOTE! this overwrite the peak.pnts with fractional peak, if a fractional value is specified.
+            # is the same of before but with frac.peak.pnts.temp, rather than with peak.pnts.temp
+            peak.pnts[k]=frac.peak.pnts.temp[frac.peak.pnts.temp>=round(msectopoints(win.ini,dim(average.temp)[1],startmsec, endmsec))&frac.peak.pnts.temp<=round(msectopoints(win.end,dim(average.temp)[1],startmsec, endmsec))][1]
+          }  # end part with fractional peak
+          
+          } else {
+          peak.pnts[k]=NA
+        }
+        
+      }
+      
+      peak.msec=pointstomsec(peak.pnts, dim(average.temp)[1], startmsec, endmsec)
+      average.temp.lat=data.frame(t(peak.msec))
+      names(average.temp.lat)=names(average.temp)#ripristino i nomi degli elettrodi, persi nei pasaggi
+      average.temp.lat$Subject=numbers[i]
+      average.temp.lat$Subject_name=Subject_name
+      datall=rbind(datall, average.temp.lat)
+    }
+    rownames(datall)=1:dim(datall)[1]
+    
+    if (format=="wide"){
+      return(datall)
+    }
+    
+    if(format=="long"){
+      
+      temp=rearrange(deps=1:(length(datall)-2),oth=c("Subject", "Subject_name"),dataset=datall, name.dep=name.dep, name.newvar=name.newvar)
+      # notice: length(datall)-2 because the last two columns are Subject and Subject_name
+    }
+    
+    if (!is.null(others))
+    {
+      for (i in 1:length(others))
+      {
+        temp[, names(others)[i]]=others[i]
+      }
+    }
+    return(temp)
+  }
--- pkg/R/import.erp.R
+++ pkg/R/import.erp.R
@@ -1,27 +1,37 @@
 #questa funzione si interfaccia con la funzione exportpicture_02_h, che esporta fil .erp che contengono erp.
 
-import.erp=function(filenamebase,numbers, ext=".txt", outname="ERP_subj", fileinfo=FALSE, erplist=NULL, path=getwd(), ...){
+import.erp=function(filenamebase,numbers, ext=".txt", outname="ERP_subj", fileinfo=FALSE, erplist=NULL, path=getwd(), electrodes = "file", ...){
 	
-	old.wd=getwd() # salvo il nome del path corrente 
-	setwd(path) #cambio path con quello specificato
-		
 	outlist=list()
 	length(outlist)=length(numbers)
 	
+	# check if the electrodes names are already in the file
+	if (electrodes[1]=="file"){
+	  myheader=TRUE # set the header to TRUE
+	} else {
+	  myheader=FALSE # set the header to FALSE
+	}
+	
 	for (i in 1:length(numbers)){
 		if (fileinfo==TRUE){
-		erpout=read.table(paste(filenamebase, numbers[i], ext ,sep=""), header=T,skip=1, ...)
+		erpout=read.table(paste(path, "/", filenamebase, numbers[i], ext ,sep=""), header=myheader,skip=1, ...)
 		erp.subjectname=readLines(paste(filenamebase, numbers[i], ext,sep=""), n=1)
 		erp.subjectname=gsub("\t","", erp.subjectname)
 		comment(erpout)=erp.subjectname
 		erpout.name=paste(outname, numbers[i], sep="")
+		  if(myheader==FALSE){ # add electrodes names if specified
+        names(erpout)=electrodes		  
+		  }
 		}
 		if (fileinfo==FALSE){
-		erpout=read.table(paste(filenamebase, numbers[i], ext ,sep=""), header=T, ...)
+		erpout=read.table(paste(path, "/", filenamebase, numbers[i], ext ,sep=""), header=myheader, ...)
 		erp.subjectname=paste(filenamebase, numbers[i], ext ,sep="")
 		erp.subjectname=gsub("\t","", erp.subjectname)
 		comment(erpout)=erp.subjectname
 		erpout.name=paste(outname, numbers[i], sep="")
+		  if(myheader==FALSE){ # add electrode names if specified
+		    names(erpout)=electrodes		  
+		  }
 		}
 	outlist[[i]]=erpout
 	names(outlist)[[i]]=erpout.name
@@ -29,8 +39,6 @@ import.erp=function(filenamebase,numbers, ext=".txt", outname="ERP_subj", filein
 	if (!is.null(erplist)){
 		outlist=c(erplist, outlist)
 	}
-
-setwd(old.wd) # ripristiono la path iniziale
 
 return(outlist)
 }
