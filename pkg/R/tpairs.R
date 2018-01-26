f0635bd3015428ddcb3b919cdd6770e874241c2d
giorgioarcara <giorgioarcara@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-01-26 08:22:33 +0000

    fixed bug tpairs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/erpr@100 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/tpairs.R
+++ pkg/R/tpairs.R
@@ -1,6 +1,6 @@
 
 tpairs = function (dat, vars, contr, dep, wid, p.adjust.methods = "none", 
-          paired = "CHECK", ...) 
+                   paired = "CHECK", ...) 
 {
   dat$newfactor = apply(data.frame(dat[, vars]), 1, function(x) {
     paste(x, collapse = "_")
@@ -49,10 +49,10 @@ tpairs = function (dat, vars, contr, dep, wid, p.adjust.methods = "none",
     }
     
     if (paired==TRUE|paired==FALSE){
-    res = t.test(dat[dat$newfactor %in% contr[[i]][[1]], 
-                     dep], dat[dat$newfactor %in% contr[[i]][[2]], dep], 
-                 paired = paired, ...)
-    pair=c(pair, paired)
+      res = t.test(dat[dat$newfactor %in% contr[[i]][[1]], 
+                       dep], dat[dat$newfactor %in% contr[[i]][[2]], dep], 
+                   paired = paired, ...)
+      pair=c(pair, paired)
     }
     contrast.names = c(contrast.names, paste(contr[[i]][[1]], 
                                              "vs", contr[[i]][[2]], sep = " "))
@@ -69,13 +69,29 @@ tpairs = function (dat, vars, contr, dep, wid, p.adjust.methods = "none",
                          2)
   
   # create splitted version of column names (a step back)
+  # case 1 (more than one variable specified)
   contr = contrast.names
-  contr_split = strsplit(as.character(contr), split= "_| ")
-  contr_mat = matrix(unlist(contr_split), byrow=TRUE, nrow = length(contr))
-  col_vs = length(vars)+1 # get the column with vs
-  contr_vars = data.frame(contr_mat[, -col_vs]) # exclude it (and convert to df)
-  old_vars_names = as.character ( t(sapply(vars, function(x){paste(x, 1:2, sep="_")})) )
-  names(contr_vars) = old_vars_names
+  
+  if (length(vars)>1){
+    contr_split = strsplit(as.character(contr), split= "_| ")
+    contr_mat = matrix(unlist(contr_split), byrow=TRUE, nrow = length(contr))
+    col_vs = length(vars)+1 # get the column with vs
+    contr_vars = data.frame(contr_mat[, -col_vs]) # exclude it (and convert to df)
+    old_vars_names = as.character ( t(sapply(vars, function(x){paste(x, 1:2, sep="_")})) )
+    names(contr_vars) = old_vars_names
+  }
+  
+  # case 2 (only one var specified)
+  # it is necessary cause the data.frame contr_vars is built slightly differently.
+  if (length(vars)==1){
+    contr_split = strsplit(as.character(contr), split= " ")
+    contr_mat = matrix(unlist(contr_split), byrow=TRUE, nrow = length(contr))
+    col_vs = length(vars)+1 # get the column with vs
+    contr_vars = data.frame(t(contr_mat[, -col_vs])) # exclude it (and convert to df)
+    old_vars_names = as.character ( t(sapply(vars, function(x){paste(x, 1:2, sep="_")})) )
+    names(contr_vars) = old_vars_names
+  }
+  
   
   results = data.frame(contr = contrast.names, p.value = p.values.corr, 
                        t.value = t.values, df = df, mean.1 = mean.1, mean.2, pair)
